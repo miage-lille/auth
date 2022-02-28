c18623d66df290593e654fc27bc20fc50a87ba08
thomas.haessle <thomas.haessle@gmail.com>
2022-02-28 15:03:01 +0000

    :construction_worker:

--- lib/api.ml
+++ lib/api.ml
@@ -1,3 +1,6 @@
+(* This Source Code Form is subject to the terms of the Mozilla Public License,
+      v. 2.0. If a copy of the MPL was not distributed with this file, You can
+      obtain one at https://mozilla.org/MPL/2.0/ *)
 let hello_handler _request =
   let open Yojson.Safe in
   Dream.json @@ to_string @@ `Assoc [("message", `String "hello world")]
